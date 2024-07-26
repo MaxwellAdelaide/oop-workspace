9683d82f92aabc1c582c9ee873a23d5838afc0d2
Maxwell0309 <2645596648@qq.com>
2024-07-26 18:31:17 +0930

    Add solution for 2-5

---
+++ Desktop/code/main-2-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+bool is_descending(int array[], int n);
+int main() {
+    int arr1[] = {5, 4, 3, 2, 1};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    std::cout << "arr1 is descending: " << std::boolalpha << is_descending(arr1, n1) << std::endl;
+
+    int arr2[] = {5, 3, 4, 2, 1};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    std::cout << "arr2 is descending: " << std::boolalpha << is_descending(arr2, n2) << std::endl;
+
+    int arr3[] = {5, 5, 5, 5, 5};
+    int n3 = sizeof(arr3) / sizeof(arr3[0]);
+    std::cout << "arr3 is descending: " << std::boolalpha << is_descending(arr3, n3) << std::endl;
+
+    int arr4[] = {};
+    int n4 = 0; 
+    std::cout << "arr4 is descending: " << std::boolalpha << is_descending(arr4, n4) << std::endl;
+    return 0;
+}
