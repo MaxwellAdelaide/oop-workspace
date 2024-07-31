cb34375620e9d9cae898e55716f5bc905ef85013
MaxwellAdelaide <a1906969@adelaide.edu.au>
2024-07-31 15:46:03 +0930

    Add files via upload
    1and2

---
+++ transform.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string DecToBin(int decNum) {
+    string binNum="";
+    while (decNum>0) {
+        int remain=decNum%2;
+        binNum=to_string(remain)+binNum;
+        decNum=decNum/2; 
+    }
+    return binNum;
+}
+
+int main() {
+    int num;
+    cout<<"Enter a decimal number: ";
+    cin>>num;
+    string binary=DecToBin(num);
+    cout<<"Binary representation: "<< binary << endl;
+    return 0;
+}
