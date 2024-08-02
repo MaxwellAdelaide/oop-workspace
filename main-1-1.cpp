68a45c91153f55c7f2845e658d0eec37595eb368
MaxwellAdelaide <a1906969@adelaide.edu.au>
2024-08-02 15:16:06 +0930

    Add files via upload
    one

---
+++ main-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "function-1-1.cpp"
+int main() {
+    int matrix[4][4]={
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+    std::cout << "Sum of diagonal elements: " << sum_diagonal(matrix) << std::endl;
+    return 0;
+}
