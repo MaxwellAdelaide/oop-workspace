3b3eca3321913df0a3b9b6d191eb16d33f354d43
MaxwellAdelaide <a1906969@adelaide.edu.au>
2024-08-02 17:27:16 +0930

    3

---
+++ main-1-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "function-1-3.cpp"
+int main() {
+    int matrix[4][4]={
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 0, 1, 2},
+        {3, 4, 5, 6}
+    };
+    count_digits(matrix);
+    return 0;
+}
